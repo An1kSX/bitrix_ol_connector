f5629d5cc53374926bf7dcdfc8c28f69af250025
Anik <admin@spletnimsu.uz>
2025-12-21 22:10:44 +0500

    bugs fixing

--- app/bitrix/service.py
+++ app/bitrix/service.py
@@ -11,7 +11,7 @@ class BitrixService:
 		self.bx24 = None
 
 	async def _get_client(self) -> Bitrix:
-		portal = await db.get(Portal, self.domain)
+		portal = await database.get(Portal, self.domain)
 
 		if not portal or not portal.token:
 			raise RuntimeError("Portal not installed")
